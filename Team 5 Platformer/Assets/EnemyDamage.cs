6240285773d8a42c0c60dc062c4f77abc47bf80c
PSmith2 <158102929+PSmith2@users.noreply.github.com>
2024-03-07 13:45:04 -0700

    Fixed player taking damage on jumping

--- Team 5 Platformer/Assets/EnemyDamage.cs
+++ Team 5 Platformer/Assets/EnemyDamage.cs
@@ -9,10 +9,28 @@ public class EnemyDamage : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
-        {
+        if (collision.gameObject.CompareTag("Player")) {
+
+            if (!IsFeetCollision(collision))
+            {
                 playerHealth.TakeDamage(damage);
-           
+
+            }
         }
     }
+
+
+    // Helper method to check if the collision involves the feet of the player
+    private bool IsFeetCollision(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.collider.CompareTag("Feet"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
